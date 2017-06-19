5698c540c863d488770e7871cdd2f40bd741e9a2
Angoranna <annabelleangoran@gmail.com>
2017-06-19 09:16:11 +0200

    tp2

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    slideWidth: 600,
+    minSlides: 2,
+    maxSlides: 6,
+    slideMargin: 10,
+    auto:true
+    
+  });
+});
+
+$(document).ready(function(){
+  $('.bxslider1').bxSlider({
+    slideWidth: 122.5,
+    minSlides: 4,
+    maxSlides: 12,
+    auto:true
+    
+  });
+});
+
+
+
+
+
+
